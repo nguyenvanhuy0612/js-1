7cc8eb118c34afddf9f1d1833021497480c494f7
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2024-07-03 22:56:40 +0700

    Update

---
+++ cucumber-automation/src/test/java/org/huy/TestCase1.java
@@ -0,0 +1,12 @@
+package org.huy;
+
+import org.junit.jupiter.api.Test;
+
+public class TestCase1 {
+
+    @Test
+    public void Test1() {
+        System.out.println("=====================Test1==================");
+        //throw new RuntimeException(this.getClass().getName());
+    }
+}
