51644b2f3047323ee693f3d0a586ab60f1cdc834
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2024-07-04 14:20:12 +0700

    update

--- cucumber-automation/src/test/java/org/huy/TestCase1.java
+++ cucumber-automation/src/test/java/org/huy/TestCase1.java
@@ -1,12 +1,34 @@
 package org.huy;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
 
 public class TestCase1 {
 
-    @Test
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("TestCase1.class.getName() = " + TestCase1.class.getName());
+    }
+
+    @Order(9001)
+    @Tag("Test_Case1")
+    @Test()
     public void Test1() {
-        System.out.println("=====================Test1==================");
+        System.out.println("=====================Test1===================");
+        System.out.println(Instant.now());
+        //throw new RuntimeException(this.getClass().getName());
+    }
+
+    @Order(9000)
+    @Tag("Test_Case2")
+    @Test()
+    public void Test2() {
+        System.out.println("=====================Test2===================");
+        System.out.println(Instant.now());
         //throw new RuntimeException(this.getClass().getName());
     }
 }
