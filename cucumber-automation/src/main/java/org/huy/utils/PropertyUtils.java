524a60698568f075f272a3bac5869a5d58ba1ace
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2024-07-07 22:43:57 +0700

    add

--- cucumber-automation/src/main/java/org/huy/utils/PropertyUtils.java
+++ cucumber-automation/src/main/java/org/huy/utils/PropertyUtils.java
@@ -1,4 +1,18 @@
 package org.huy.utils;
 
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class PropertyUtils {
+
+    public static Properties read(String filePath) {
+        Properties prop = new Properties();
+        try (FileInputStream input = new FileInputStream(filePath)) {
+            prop.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
 }
